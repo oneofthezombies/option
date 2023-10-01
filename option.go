70d6cce8945e8f30deef1fd7514121066284bf27
oneofthezombies <hunhoekim@gmail.com>
2023-10-02 00:06:51 +0900

    chore: remove reference

--- option.go
+++ option.go
@@ -4,7 +4,7 @@ type Option[T any] struct {
 	value *T
 }
 
-func (o *Option[T]) Value() T {
+func (o Option[T]) Value() T {
 	if o.value == nil {
 		panic("value is nil. use Has() to check if value is present")
 	}
@@ -12,18 +12,18 @@ func (o *Option[T]) Value() T {
 	return *o.value
 }
 
-func (o *Option[T]) Has() bool {
+func (o Option[T]) Has() bool {
 	return o.value != nil
 }
 
-func Some[T any](value T) *Option[T] {
-	return &Option[T]{
+func Some[T any](value T) Option[T] {
+	return Option[T]{
 		value: &value,
 	}
 }
 
-func None[T any]() *Option[T] {
-	return &Option[T]{
+func None[T any]() Option[T] {
+	return Option[T]{
 		value: nil,
 	}
 }
