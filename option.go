a7bb9f74399f02c9c23b157acce80289be045a9f
oneofthezombies <hunhoekim@gmail.com>
2023-10-02 04:34:57 +0900

    chore: remove panic

--- option.go
+++ option.go
@@ -1,29 +1,27 @@
 package option
 
 type Option[T any] struct {
-	value *T
+	holder []T
+	ok     bool
 }
 
-func (o Option[T]) Value() T {
-	if o.value == nil {
-		panic("value is nil. use Has() to check if value is present")
-	}
-
-	return *o.value
-}
-
-func (o Option[T]) Has() bool {
-	return o.value != nil
+func (o Option[T]) Value() (T, bool) {
+	return o.holder[0], o.ok
 }
 
 func Some[T any](value T) Option[T] {
+	holder := make([]T, 1)
+	holder[0] = value
+
 	return Option[T]{
-		value: &value,
+		ok:     true,
+		holder: holder,
 	}
 }
 
 func None[T any]() Option[T] {
 	return Option[T]{
-		value: nil,
+		ok:     false,
+		holder: make([]T, 1),
 	}
 }
--- option_test.go
+++ option_test.go
@@ -8,28 +8,65 @@ import (
 
 func TestSome(t *testing.T) {
 	o := Some(1)
-
-	if !o.Has() {
+	v, ok := o.Value()
+	if !ok {
 		t.Error("expected option to have value")
 	}
 
-	if o.Value() != 1 {
+	if v != 1 {
 		t.Error("expected option value to be 1")
 	}
 }
 
 func TestNone(t *testing.T) {
 	o := None[int]()
-
-	if o.Has() {
+	v, ok := o.Value()
+	if ok {
 		t.Error("expected option to not have value")
 	}
 
-	defer func() {
-		if r := recover(); r == nil {
-			t.Error("expected panic")
-		}
-	}()
+	if v != 0 {
+		t.Error("expected option value to be 0")
+	}
+}
 
-	o.Value()
+func TestNoneString(t *testing.T) {
+	o := None[string]()
+	v, ok := o.Value()
+	if ok {
+		t.Error("expected option to not have value")
+	}
+
+	if v != "" {
+		t.Error("expected option value to be empty string")
+	}
+}
+
+func TestNoneEmptyStruct(t *testing.T) {
+	o := None[struct{}]()
+	v, ok := o.Value()
+	if ok {
+		t.Error("expected option to not have value")
+	}
+
+	if v != (struct{}{}) {
+		t.Error("expected option value to be empty struct")
+	}
+}
+
+func TestNoneStruct(t *testing.T) {
+	type S struct {
+		A int
+		B string
+	}
+
+	o := None[S]()
+	v, ok := o.Value()
+	if ok {
+		t.Error("expected option to not have value")
+	}
+
+	if v != (S{}) {
+		t.Error("expected option value to be empty struct")
+	}
 }
